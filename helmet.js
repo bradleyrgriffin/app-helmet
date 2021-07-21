699a2487262dec04b721a3f5ecc0b499005ee3d0
Brad <36484043+bradleyrgriffin@users.noreply.github.com>
2021-07-21 14:47:53 -0400

    Update helmet.js

--- helmet.js
+++ helmet.js
@@ -4,3 +4,4 @@
   var masthead = $('<div class="header"><a href="#default" class="logo">CompanyLogo</a><div class="header-right"><a class="active" href="#home">Home</a><a href="#contact">Contact</a><a href="#about">About</a></div></div>');
     $('body').prepend(masthead);
   });
+});
