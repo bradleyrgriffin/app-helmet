f55f03e12f4d950e5635893feedd36320fe068f7
Brad <36484043+bradleyrgriffin@users.noreply.github.com>
2021-07-21 14:47:11 -0400

    Update helmet.js

--- helmet.js
+++ helmet.js
@@ -1,13 +1,6 @@
 (function($) {
   "use strict";
   $(window).on("load", function() { // makes sure the whole site is loaded
-  var masthead = $(<div class="header">
-                      <a href="#default" class="logo">CompanyLogo</a>
-                    <div class="header-right">
-                      <a class="active" href="#home">Home</a>
-                      <a href="#contact">Contact</a>
-                      <a href="#about">About</a>
-                    </div>
-                   </div>);
+  var masthead = $('<div class="header"><a href="#default" class="logo">CompanyLogo</a><div class="header-right"><a class="active" href="#home">Home</a><a href="#contact">Contact</a><a href="#about">About</a></div></div>');
     $('body').prepend(masthead);
   });
